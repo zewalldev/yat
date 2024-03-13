6aaba62904054d29b7c6edcf25a7e6bfe58505f9
Denis Shirokov <de.sh.vit@gmail.com>
2024-03-13 09:25:13 +0300

    move nondot to do notation

--- src/Core.hs
+++ src/Core.hs
@@ -2,11 +2,11 @@ module Core (doInit, newTodo, startTodo, finishTodo) where
 
 import Data.Bool (bool)
 import Entity (TaskName)
-import System.Directory (copyFile, createDirectoryIfMissing, doesDirectoryExist, getCurrentDirectory, renameFile)
+import System.Directory (copyFile, createDirectoryIfMissing, doesDirectoryExist, doesFileExist, getCurrentDirectory, renameFile)
 import System.FilePath (takeDirectory, (<.>), (</>))
 import System.IO (hFlush, hPutStr)
 import System.IO.Temp (withSystemTempFile)
-import System.Process (createProcess, shell, waitForProcess)
+import System.Process (runCommand, waitForProcess)
 
 yatDir, trackDir, newDir, inprogressDir, doneDir, releaseDir, releasedDir :: FilePath
 yatDir = ".yat"
@@ -34,17 +34,16 @@ findYatRootFrom :: FilePath -> IO (Maybe FilePath)
 findYatRootFrom "/" = doesDirectoryExist ("/" </> yatDir) >>= bool (return Nothing) (return . Just $ "/")
 findYatRootFrom path = doesDirectoryExist (path </> yatDir) >>= bool (findYatRootFrom . takeDirectory $ path) (return . Just $ path)
 
-findYatRootFromCurrentAnd :: (FilePath -> IO ()) -> IO ()
-findYatRootFromCurrentAnd handle = getCurrentDirectory >>= findYatRootFrom >>= maybe notInitialized handle
+findYatRootFromCurrent :: IO (Maybe FilePath)
+findYatRootFromCurrent = getCurrentDirectory >>= findYatRootFrom
 
 createYatDirectories :: IO ()
-createYatDirectories =
-  createDirectoryIfMissing True newPath
-    >> createDirectoryIfMissing True inprogressPath
-    >> createDirectoryIfMissing True donePath
-    >> createDirectoryIfMissing True releasePath
-    >> createDirectoryIfMissing True releasedPath
-    >> putStrLn "Yat has initialized."
+createYatDirectories = do 
+    createDirectoryIfMissing True newPath
+    createDirectoryIfMissing True inprogressPath
+    createDirectoryIfMissing True donePath
+    createDirectoryIfMissing True releasePath
+    createDirectoryIfMissing True releasedPath
 
 editorcmd :: String
 editorcmd = "vim"
@@ -55,27 +54,70 @@ todoTemaplate = unlines ["@title: ", "@author: ", "@implementor: ", "@descriptio
 createTodo :: FilePath -> IO ()
 createTodo todoPath = do
   withSystemTempFile "yat.todo" $ \src hsrc -> do
-    putStrLn src
-    hPutStr hsrc todoTemaplate >> hFlush hsrc
-    (_, _, _, h) <- createProcess (shell (unwords [editorcmd, src]))
-    r <- waitForProcess h
-    print r
+    hPutStr hsrc todoTemaplate
+    hFlush hsrc
+    _ <- runCommand (unwords [editorcmd, src]) >>= waitForProcess
     copyFile src todoPath
 
 doInit :: IO ()
-doInit = doesDirectoryExist yatDir >>= bool createYatDirectories alreadyInitialized
+doInit = do 
+    alreadyInit <- doesDirectoryExist yatDir
+    if not alreadyInit
+        then do 
+            createYatDirectories
+            putStrLn "Yat has initialized."
+        else
+            alreadyInitialized
 
 newTodo :: TaskName -> IO ()
-newTodo name = findYatRootFromCurrentAnd (createTodo . (</> newPath </> name <.> "todo")) >> putStrLn ("Todo " ++ name ++ " is created")
+newTodo name = do
+  mb_rootDir <- findYatRootFromCurrent
+  case mb_rootDir of
+    Nothing -> notInitialized
+    Just rootDir -> do
+      let new = rootDir </> newPath </> name <.> "todo"
+      alreadyEixsts <- doesFileExist new
+      if not alreadyEixsts
+        then do
+          createTodo new
+          putStrLn ("Todo " ++ name ++ " is created")
+        else
+          putStrLn ("Todo " ++ name ++ " already exists")
 
 startTodo :: TaskName -> IO ()
-startTodo name = findYatRootFromCurrentAnd $
-  \root ->
-    renameFile (root </> newPath </> name <.> "todo") (root </> inprogressPath </> name <.> "todo")
-      >> putStrLn ("Todo " ++ name ++ " is starting")
+startTodo name = do
+  mb_rootDir <- findYatRootFromCurrent
+  case mb_rootDir of
+    Nothing -> notInitialized
+    Just rootDir -> do
+      let from = rootDir </> newPath </> name <.> "todo"
+      let to = rootDir </> inprogressPath </> name <.> "todo"
+      todoExists <- doesFileExist from
+      todoInprogress <- doesFileExist to
+      if todoExists
+        then
+          if not todoInprogress
+            then do
+              renameFile from to
+              putStrLn ("Todo " ++ name ++ " is starting")
+            else putStrLn "Todo already in progress."
+        else putStrLn "Todo doesn't exists"
 
 finishTodo :: TaskName -> IO ()
-finishTodo name = findYatRootFromCurrentAnd $
-  \root ->
-    renameFile (root </> inprogressPath </> name <.> "todo") (root </> donePath </> name <.> "todo")
-      >> putStrLn ("Todo " ++ name ++ " is finished")
+finishTodo name = do
+  mb_rootDir <- findYatRootFromCurrent
+  case mb_rootDir of
+    Nothing -> notInitialized
+    Just rootDir -> do
+      let from = rootDir </> inprogressPath </> name <.> "todo"
+      let to = rootDir </> donePath </> name <.> "todo"
+      todoInProgress <- doesFileExist from
+      todoIsDone <- doesFileExist to
+      if todoInProgress
+        then
+          if not todoIsDone
+            then do
+              renameFile from to
+              putStrLn ("Todo " ++ name ++ " is finished")
+            else putStrLn "Todo is already done."
+        else putStrLn "Todo doesn't exists"
